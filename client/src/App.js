c2fde1de8f579dd0199033e3272a9695f9d15b6a
Edward Hong <edward.hong527@gmail.com>
2020-10-15 09:41:29 +1300

    delete functionality

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,39 @@ import Container from '@material-ui/core/Container'
 import Typography from '@material-ui/core/Typography'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
+import ButtonGroup from '@material-ui/core/ButtonGroup'
 import Grid from '@material-ui/core/Grid'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
 import AddIcon from '@material-ui/icons/Add'
+import CheckIcon from '@material-ui/icons/Check'
+import CreateIcon from '@material-ui/icons/Create'
+import DeleteIcon from '@material-ui/icons/Delete'
+import {
+  createMuiTheme,
+  MuiThemeProvider,
+  withStyles,
+} from '@material-ui/core/styles'
+import green from '@material-ui/core/colors/green'
+import yellow from '@material-ui/core/colors/yellow'
+import red from '@material-ui/core/colors/red'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: green,
+    secondary: yellow,
+  },
+})
+
+const DeleteButton = withStyles({
+  root: {
+    background: red[500],
+    '&:hover': {
+      background: red[600],
+    },
+  },
+})(Button)
 
 const App = () => {
   const [newTodo, setNewTodo] = useState('')
@@ -23,6 +51,10 @@ const App = () => {
     if (e.key === 'Enter') {
       addTodo()
     }
+  }
+
+  const handleDelete = (i) => () => {
+    setTodos([...todos.slice(0, i), ...todos.slice(i + 1)])
   }
 
   return (
@@ -43,18 +75,35 @@ const App = () => {
           />
         </Grid>
         <Grid item xs={1}>
-          <Button onClick={addTodo} variant="contained" color="primary">
+          <Button onClick={addTodo} variant="contained">
             <AddIcon />
           </Button>
         </Grid>
         <Grid item xs={12}>
-          <List>
-            {todos.map((todo, i) => (
-              <ListItem divider key={i}>
-                <ListItemText primary={todo} />
-              </ListItem>
-            ))}
-          </List>
+          <MuiThemeProvider theme={theme}>
+            <List>
+              {todos.map((todo, i) => (
+                <ListItem divider key={i}>
+                  <ListItemText primary={todo} />
+
+                  <ButtonGroup
+                    variant="contained"
+                    aria-label="contained primary button group"
+                  >
+                    <Button color="primary">
+                      <CheckIcon />
+                    </Button>
+                    <Button color="secondary">
+                      <CreateIcon />
+                    </Button>
+                    <DeleteButton onClick={handleDelete(i)} color="default">
+                      <DeleteIcon />
+                    </DeleteButton>
+                  </ButtonGroup>
+                </ListItem>
+              ))}
+            </List>
+          </MuiThemeProvider>
         </Grid>
       </Grid>
     </Container>
