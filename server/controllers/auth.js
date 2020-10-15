5cd2603849dd3a8c6f8fb9e3a4d970ba4252b085
Edward Hong <edward.hong527@gmail.com>
2020-10-16 12:40:25 +1300

    signup controller

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,29 @@
+const User = require('../models/user')
+
 exports.signup = (req, res) => {
-  console.log('REQ BODY ON SIGNUP', req.body)
-  res.json({
-    data: 'You hit signup endpoint',
+  const { name, email, password } = req.body
+
+  User.findOne({ email }).then((foundUser) => {
+    if (foundUser) {
+      return res.status(400).json({
+        error: 'Email is taken',
+      })
+    }
+
+    const newUser = new User({ name, email, password })
+
+    newUser
+      .save()
+      .then(() => {
+        res.json({
+          message: 'Signup success! Please signin',
+        })
+      })
+      .catch((error) => {
+        console.log('SIGNUP ERROR', error)
+        return res.status(400).json({
+          errror,
+        })
+      })
   })
 }
