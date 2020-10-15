03fa3a8b1d505d78b7a11b776e322d90b299223e
Edward Hong <edward.hong527@gmail.com>
2020-10-16 09:29:01 +1300

    apply middlewares

--- server/server.js
+++ server/server.js
@@ -1,8 +1,19 @@
+require('dotenv').config()
 const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const mongoose = require('mongoose')
 
 const authRoutes = require('./routes/auth')
 
 const app = express()
+
+app.use(morgan('dev'))
+app.use(express.json())
+
+if ((process.env.NODE_ENV = 'development')) {
+  app.use(cors({ origin: process.env.CLIENT_URL }))
+}
 
 app.use('/api', authRoutes)
 
