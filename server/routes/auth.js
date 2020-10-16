bc166c356f1ff2d4c003b6f79b4e90a8e27f1034
Edward Hong <edward.hong527@gmail.com>
2020-10-16 13:24:36 +1300

    save user to db after email activation

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-const { signup } = require('../controllers/auth')
+const { signup, accountActivation } = require('../controllers/auth')
 const { userSignupValidator } = require('../validators/auth')
 const { runValidation } = require('../validators')
 
 router.post('/signup', userSignupValidator, runValidation, signup)
+router.post('/activation', accountActivation)
 
 module.exports = router
