fc0e953836c47eb395dbe8d52e1e12f1f760ad24
Edward Hong <edward.hong527@gmail.com>
2020-10-16 20:10:55 +1300

    only send activation email from backend when email doesn't exist

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -42,41 +42,41 @@ exports.signup = (req, res) => {
       return res.status(400).json({
         error: 'Email is taken',
       })
+    } else {
+      const token = jwt.sign(
+        { name, email, password },
+        process.env.JWT_ACCOUNT_ACTIVATION,
+        { expiresIn: '10m' },
+      )
+
+      const emailData = {
+        from: process.env.EMAIL_FROM,
+        to: email,
+        subject: 'Account activation link',
+        html: `
+          <h1>Please use the following link to activate your account</h1>
+          <p>${process.env.CLIENT_URL}/auth/activate/${token}</p>
+          <hr />
+          <p>This email may contain sensitive information</p>
+          <p>${process.env.CLIENT_URL}</p>
+        `,
+      }
+
+      sgMail
+        .send(emailData)
+        .then((sent) => {
+          return res.json({
+            message: `Email has been sent to ${email}. Follow the instructions to activate your account.`,
+          })
+        })
+        .catch((err) => {
+          console.log('SIGNUP EMAIL SENT ERROR', err)
+          return res.json({
+            message: err.message,
+          })
+        })
     }
   })
-
-  const token = jwt.sign(
-    { name, email, password },
-    process.env.JWT_ACCOUNT_ACTIVATION,
-    { expiresIn: '10m' },
-  )
-
-  const emailData = {
-    from: process.env.EMAIL_FROM,
-    to: email,
-    subject: 'Account activation link',
-    html: `
-      <h1>Please use the following link to activate your account</h1>
-      <p>${process.env.CLIENT_URL}/auth/activate/${token}</p>
-      <hr />
-      <p>This email may contain sensitive information</p>
-      <p>${process.env.CLIENT_URL}</p>
-    `,
-  }
-
-  sgMail
-    .send(emailData)
-    .then((sent) => {
-      return res.json({
-        message: `Email has been sent to ${email}. Follow the instructions to activate your account.`,
-      })
-    })
-    .catch((err) => {
-      console.log('SIGNUP EMAIL SENT ERROR', err)
-      return res.json({
-        message: err.message,
-      })
-    })
 }
 
 exports.accountActivation = (req, res) => {
