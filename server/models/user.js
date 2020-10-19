f76d40a331f47bbd9a39dbd3997552d7fe449127
Edward Hong <edward.hong527@gmail.com>
2020-10-19 15:44:18 +1300

    remove role from user model

--- server/models/user.js
+++ server/models/user.js
@@ -21,10 +21,6 @@ const userSchema = new mongoose.Schema(
       required: true,
     },
     salt: String,
-    role: {
-      type: String,
-      default: 'subscriber',
-    },
     resetPasswordLink: {
       data: String,
       default: '',
