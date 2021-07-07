c856ee6a9348eea2041f31c0a071d533e60dfae2
Edward Hong <edward.hong527@gmail.com>
2021-07-08 04:35:04 +1200

    redirect after signin

--- client/src/Signin/index.jsx
+++ client/src/Signin/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Redirect, Link } from 'react-router-dom'
+import { Redirect, Link, useHistory } from 'react-router-dom'
 import Container from '@material-ui/core/Container'
 import Typography from '@material-ui/core/Typography'
 import TextField from '@material-ui/core/TextField'
@@ -12,95 +12,95 @@ import useToast from '../hooks/useToast'
 import { authenticate, isAuth } from '../utils/helpers'
 
 const Signin = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [
-    open,
-    setOpen,
-    severity,
-    setSeverity,
-    toastMsg,
-    setToastMsg,
-    handleClose,
-  ] = useToast()
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+	const history = useHistory()
+	const [
+		open,
+		setOpen,
+		severity,
+		setSeverity,
+		toastMsg,
+		setToastMsg,
+		handleClose,
+	] = useToast()
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
+	const handleSubmit = e => {
+		e.preventDefault()
 
-    axios({
-      method: 'POST',
-      url: '/auth/signin',
-      data: { email, password },
-    })
-      .then((response) => {
-        authenticate(response)
-      })
-      .catch((error) => {
-        setSeverity('error')
-        setToastMsg(error.response.data.error)
-        setOpen(true)
-      })
-  }
+		axios({
+			method: 'POST',
+			url: '/auth/signin',
+			data: { email, password },
+		})
+			.then(response => {
+				authenticate(response)
+				history.push('/')
+			})
+			.catch(error => {
+				setSeverity('error')
+				setToastMsg(error.response.data.error)
+				setOpen(true)
+			})
+	}
 
-  return (
-    <Container maxWidth="sm">
-      <Typography
-        data-testid="heading"
-        align="center"
-        variant="h2"
-        component="h1"
-      >
-        Signin
-      </Typography>
-      <form onSubmit={handleSubmit}>
-        <Grid container spacing={1}>
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              inputProps={{ 'data-testid': 'email' }}
-              label="Email"
-              variant="outlined"
-              size="small"
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              inputProps={{ 'data-testid': 'password' }}
-              label="Password"
-              variant="outlined"
-              size="small"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Grid>
-          <Button
-            data-testid="submit"
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-          >
-            Signin
-          </Button>
-          <Typography data-testid="forgot" align="right" component="p">
-            Forgot Password? Click <Link to="/forgot">here</Link>
-          </Typography>
-        </Grid>
-      </form>
-      <Toast
-        open={open}
-        handleClose={handleClose}
-        severity={severity}
-        toastMsg={toastMsg}
-      />
-      {isAuth() && <Redirect to="/" />}
-    </Container>
-  )
+	return (
+		<Container maxWidth="sm">
+			<Typography
+				data-testid="heading"
+				align="center"
+				variant="h2"
+				component="h1">
+				Signin
+			</Typography>
+			<form onSubmit={handleSubmit}>
+				<Grid container spacing={1}>
+					<Grid item xs={12}>
+						<TextField
+							fullWidth
+							inputProps={{ 'data-testid': 'email' }}
+							label="Email"
+							variant="outlined"
+							size="small"
+							type="email"
+							value={email}
+							onChange={e => setEmail(e.target.value)}
+						/>
+					</Grid>
+					<Grid item xs={12}>
+						<TextField
+							fullWidth
+							inputProps={{ 'data-testid': 'password' }}
+							label="Password"
+							variant="outlined"
+							size="small"
+							type="password"
+							value={password}
+							onChange={e => setPassword(e.target.value)}
+						/>
+					</Grid>
+					<Button
+						data-testid="submit"
+						type="submit"
+						fullWidth
+						variant="contained"
+						color="primary">
+						Signin
+					</Button>
+					<Typography data-testid="forgot" align="right" component="p">
+						Forgot Password? Click <Link to="/forgot">here</Link>
+					</Typography>
+				</Grid>
+			</form>
+			<Toast
+				open={open}
+				handleClose={handleClose}
+				severity={severity}
+				toastMsg={toastMsg}
+			/>
+			{isAuth() && <Redirect to="/" />}
+		</Container>
+	)
 }
 
 export default Signin
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -6,232 +6,233 @@ const User = require('../models/user')
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 exports.signup = (req, res) => {
-  const { name, email, password } = req.body
+	const { name, email, password } = req.body
 
-  User.findOne({ email }).then((foundUser) => {
-    if (foundUser) {
-      return res.status(400).json({
-        error: 'Email is taken',
-      })
-    } else {
-      const token = jwt.sign(
-        { name, email, password },
-        process.env.JWT_ACCOUNT_ACTIVATION,
-        { expiresIn: '10m' },
-      )
+	User.findOne({ email }).then(foundUser => {
+		if (foundUser) {
+			return res.status(400).json({
+				error: 'Email is taken',
+			})
+		} else {
+			const token = jwt.sign(
+				{ name, email, password },
+				process.env.JWT_ACCOUNT_ACTIVATION,
+				{ expiresIn: '10m' }
+			)
 
-      const emailData = {
-        from: process.env.EMAIL_FROM,
-        to: email,
-        subject: 'Account activation link',
-        html: `
+			const emailData = {
+				from: process.env.EMAIL_FROM,
+				to: email,
+				subject: 'Account activation link',
+				html: `
           <h1>Please use the following link to activate your account</h1>
           <p>${process.env.CLIENT_URL}/activate/${token}</p>
           <hr />
           <p>This email may contain sensitive information</p>
           <p>${process.env.CLIENT_URL}</p>
         `,
-      }
+			}
 
-      sgMail
-        .send(emailData)
-        .then((sent) => {
-          return res.json({
-            message: `Email has been sent to ${email}. Follow the instructions to activate your account.`,
-          })
-        })
-        .catch((err) => {
-          console.log('SIGNUP EMAIL SENT ERROR', err)
-          return res.json({
-            message: err.message,
-          })
-        })
-    }
-  })
+			sgMail
+				.send(emailData)
+				.then(sent => {
+					return res.json({
+						message: `Email has been sent to ${email}. Follow the instructions to activate your account.`,
+					})
+				})
+				.catch(err => {
+					console.log('SIGNUP EMAIL SENT ERROR', err)
+					return res.json({
+						message: err.message,
+					})
+				})
+		}
+	})
 }
 
 exports.accountActivation = (req, res) => {
-  const { token } = req.body
+	const { token } = req.body
 
-  if (token) {
-    jwt.verify(token, process.env.JWT_ACCOUNT_ACTIVATION, function (
-      err,
-      decoded,
-    ) {
-      if (err) {
-        console.log('JWT VERIFY IN ACCOUNT ACTIVATION ERROR', err)
-        return res.status(401).json({
-          error: 'Expired link. Signup again',
-        })
-      }
+	if (token) {
+		jwt.verify(
+			token,
+			process.env.JWT_ACCOUNT_ACTIVATION,
+			function (err, decoded) {
+				if (err) {
+					console.log('JWT VERIFY IN ACCOUNT ACTIVATION ERROR', err)
+					return res.status(401).json({
+						error: 'Expired link. Signup again',
+					})
+				}
 
-      const { name, email, password } = jwt.decode(token)
+				const { name, email, password } = jwt.decode(token)
 
-      const newUser = new User({ name, email, password })
+				const newUser = new User({ name, email, password })
 
-      newUser
-        .save()
-        .then(() => {
-          return res.json({
-            message: 'Signup success. Please signin',
-          })
-        })
-        .catch((err) => {
-          if (err) {
-            console.log('SAVE USER IN ACCOUNT ACTIVATION ERROR', err)
-            return res.status(401).json({
-              error: 'Error saving user in database. Try signup again',
-            })
-          }
-        })
-    })
-  } else {
-    return res.json({
-      message: 'Something went wrong. Try again.',
-    })
-  }
+				newUser
+					.save()
+					.then(() => {
+						return res.json({
+							message: 'Signup success. Please signin',
+						})
+					})
+					.catch(err => {
+						if (err) {
+							console.log('SAVE USER IN ACCOUNT ACTIVATION ERROR', err)
+							return res.status(401).json({
+								error: 'Error saving user in database. Try signup again',
+							})
+						}
+					})
+			}
+		)
+	} else {
+		return res.json({
+			message: 'Something went wrong. Try again.',
+		})
+	}
 }
 
 exports.signin = (req, res) => {
-  const { email, password } = req.body
+	const { email, password } = req.body
 
-  User.findOne({ email })
-    .then((foundUser) => {
-      if (!foundUser) {
-        return res.status(400).json({
-          error: 'User with that email does not exist. Please signup',
-        })
-      }
+	User.findOne({ email })
+		.then(foundUser => {
+			if (!foundUser) {
+				return res.status(400).json({
+					error: 'User with that email does not exist. Please signup',
+				})
+			}
 
-      // authenticate
-      if (!foundUser.authenticate(password)) {
-        return res.status(400).json({
-          error: 'Email and password do not match',
-        })
-      }
+			// authenticate
+			if (!foundUser.authenticate(password)) {
+				return res.status(400).json({
+					error: 'Email and password do not match',
+				})
+			}
 
-      // generate a token and send to client
-      const token = jwt.sign({ _id: foundUser._id }, process.env.JWT_SECRET, {
-        expiresIn: '7d',
-      })
-      const { _id, name, email, role } = foundUser
+			// generate a token and send to client
+			const token = jwt.sign({ _id: foundUser._id }, process.env.JWT_SECRET, {
+				expiresIn: '7d',
+			})
+			const { _id, name, email, role } = foundUser
 
-      return res.json({
-        token,
-        user: { _id, name, email, role },
-      })
-    })
-    .catch((err) => {
-      console.log('SIGNIN USER ERROR', err)
-      return res.json({
-        error: 'User with that email does not exist. Please signup',
-      })
-    })
+			return res.json({
+				token,
+				user: { _id, name, email, role },
+			})
+		})
+		.catch(err => {
+			console.log('SIGNIN USER ERROR', err)
+			return res.json({
+				error: 'User with that email does not exist. Please signup',
+			})
+		})
 }
 
 exports.forgotPassword = (req, res) => {
-  const { email } = req.body
+	const { email } = req.body
 
-  User.findOne({ email })
-    .then((foundUser) => {
-      if (!foundUser) {
-        return res.status(400).json({
-          error: 'User with that email does not exist',
-        })
-      } else {
-        const token = jwt.sign(
-          { _id: foundUser._id },
-          process.env.JWT_RESET_PASSWORD,
-          { expiresIn: '10m' },
-        )
+	User.findOne({ email })
+		.then(foundUser => {
+			if (!foundUser) {
+				return res.status(400).json({
+					error: 'User with that email does not exist',
+				})
+			} else {
+				const token = jwt.sign(
+					{ _id: foundUser._id },
+					process.env.JWT_RESET_PASSWORD,
+					{ expiresIn: '10m' }
+				)
 
-        const emailData = {
-          from: process.env.EMAIL_FROM,
-          to: email,
-          subject: 'Password Reset Link',
-          html: `
+				const emailData = {
+					from: process.env.EMAIL_FROM,
+					to: email,
+					subject: 'Password Reset Link',
+					html: `
             <h1>Please use the following link to reset your password</h1>
             <p>${process.env.CLIENT_URL}/reset/${token}</p>
             <hr />
             <p>This email may contain sensitive information</p>
             <p>${process.env.CLIENT_URL}</p>
           `,
-        }
+				}
 
-        return foundUser
-          .updateOne({ resetPasswordLink: token })
-          .then(() => {
-            sgMail
-              .send(emailData)
-              .then((sent) => {
-                return res.json({
-                  message: `Email has been sent to ${email}. Follow the instructions to reset your password.`,
-                })
-              })
-              .catch((err) => {
-                console.log('SIGNUP EMAIL SENT ERROR', err)
-                return res.json({
-                  message: err.message,
-                })
-              })
-          })
-          .catch(() => {
-            return res.status(400).json({
-              error:
-                'Database connection error on user password forgot request',
-            })
-          })
-      }
-    })
-    .catch((err) => {
-      return res.status(400).json({
-        error: 'User with that email does not exist',
-      })
-    })
+				return foundUser
+					.updateOne({ resetPasswordLink: token })
+					.then(() => {
+						sgMail
+							.send(emailData)
+							.then(sent => {
+								return res.json({
+									message: `Email has been sent to ${email}. Follow the instructions to reset your password.`,
+								})
+							})
+							.catch(err => {
+								console.log('SIGNUP EMAIL SENT ERROR', err)
+								return res.json({
+									message: err.message,
+								})
+							})
+					})
+					.catch(() => {
+						return res.status(400).json({
+							error:
+								'Database connection error on user password forgot request',
+						})
+					})
+			}
+		})
+		.catch(err => {
+			return res.status(400).json({
+				error: 'User with that email does not exist',
+			})
+		})
 }
 
 exports.resetPassword = (req, res) => {
-  const { resetPasswordLink, newPassword } = req.body
+	const { resetPasswordLink, newPassword } = req.body
 
-  if (resetPasswordLink) {
-    jwt.verify(
-      resetPasswordLink,
-      process.env.JWT_RESET_PASSWORD,
-      (err, decoded) => {
-        if (err) {
-          return res.status(400).json({
-            error: 'Expired link. Try again',
-          })
-        }
+	if (resetPasswordLink) {
+		jwt.verify(
+			resetPasswordLink,
+			process.env.JWT_RESET_PASSWORD,
+			(err, decoded) => {
+				if (err) {
+					return res.status(400).json({
+						error: 'Expired link. Try again',
+					})
+				}
 
-        User.findOne({ resetPasswordLink })
-          .then((foundUser) => {
-            if (!foundUser) {
-              return res.status(400).json({
-                error: 'Could not find user with reset password link',
-              })
-            }
+				User.findOne({ resetPasswordLink })
+					.then(foundUser => {
+						if (!foundUser) {
+							return res.status(400).json({
+								error: 'Could not find user with reset password link',
+							})
+						}
 
-            foundUser.password = newPassword
-            foundUser.resetPasswordLink = ''
+						foundUser.password = newPassword
+						foundUser.resetPasswordLink = ''
 
-            foundUser
-              .save()
-              .then((result) => {
-                res.json({
-                  message: 'Great! Now you can login with your new password',
-                })
-              })
-              .catch((err) => {
-                res.status(400).json({ error: 'Error resetting user password' })
-              })
-          })
-          .catch((err) => {
-            return res.status(400).json({
-              error: 'Something went wrong. Try later',
-            })
-          })
-      },
-    )
-  }
+						foundUser
+							.save()
+							.then(result => {
+								res.json({
+									message: 'Great! Now you can login with your new password',
+								})
+							})
+							.catch(err => {
+								res.status(400).json({ error: 'Error resetting user password' })
+							})
+					})
+					.catch(err => {
+						return res.status(400).json({
+							error: 'Something went wrong. Try later',
+						})
+					})
+			}
+		)
+	}
 }
